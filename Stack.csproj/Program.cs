f652cba46ec0f571601864d373af48ce5e89ed29
Ryan Bresnahan <r.bresnahan@hotmail.com>
2022-07-04 12:00:34 -0600

    Stack test

--- Stack.csproj/Program.cs
+++ Stack.csproj/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections;
 
-namespace Stack.csproj
+namespace StackTest.csproj
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack myStack = new Stack();
+            myStack.Push("Hello");
+            myStack.Push("Space");
+            myStack.Pop();
+            myStack.Push("World");
+            myStack.Push("!");
+
+        // Displays the properties and values of the Stack.
+        Console.WriteLine( "myStack" );
+        Console.WriteLine( "\tCount:    {0}", myStack.Count );
+        Console.Write( "\tValues:" );
+        PrintValues( myStack );
+        }
+
+        public static void PrintValues( IEnumerable myCollection ) {
+            foreach (Object obj in myCollection )
+                Console.Write("    {0}", obj);
+            Console.WriteLine();
         }
     }
 }
