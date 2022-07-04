4c2d03ab286fb3938cb3f609ae9a3a21fc82bc97
Ryan Bresnahan <r.bresnahan@hotmail.com>
2022-07-04 11:33:48 -0600

    Project setup

---
+++ HashTable.csproj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTable.csproj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
